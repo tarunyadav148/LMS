714aca1c6c8b0cc55c4176ebb86b814ef0b7a64a
Tarun Singh Yadav <tarunyadav148@gmail.com>
2021-01-31 21:58:26 +0530

    remove definations of methods

--- student.cpp
+++ student.cpp
@@ -1,38 +1,33 @@
+#include"student.h"
 #include<iostream>
 #include<string>
 using namespace std;
 
-class student
+
+void student::sinput()
 {
-int addmno;
-string sname;
-int Class;
-char sec;
-int rno;
-public:
-void sinput()
-{
-cout<<"Enter Addmission No.:";
-cin>>addmno;
-cout<<"Enter Name of student:";
-getline(cin,sname);
-cout<<"Enter class of student:";
-cin>>Class;
-cout<<"Enter section of student:";
-cin>>sec;
-cout<<"Enter Roll of student:";
-cin>>rno;
+    cout<<"Enter Addmission No.:";
+    cin>>addmno;
+    cout<<"Enter Name of student:";
+    getline(cin,sname);
+    cout<<"Enter class of student:";
+    cin>>Class;
+    cout<<"Enter section of student:";
+    cin>>sec;
+    cout<<"Enter Roll of student:";
+    cin>>rno;
 }
-void sdisplay()
+
+void student::sdisplay()
 {
-cout<<"Name:"<<sname<<endl;
-cout<<"Class:"<<Class<<endl;
-cout<<"Section:"<<sec<<endl;
-cout<<"Addmission Number:"<<addmno<<endl;
-cout<<"Roll Number:"<<rno<<endl;
+    cout<<"Name:"<<sname<<endl;
+    cout<<"Class:"<<Class<<endl;
+    cout<<"Section:"<<sec<<endl;
+    cout<<"Addmission Number:"<<addmno<<endl;
+    cout<<"Roll Number:"<<rno<<endl;
 }
-int getaddmno()
+
+int student::getaddmno()
 {
-return addmno;
+    return addmno;
 }
-};
--- student.h
+++ student.h
@@ -1,43 +1,20 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include<iostream>
 #include<string>
-using namespace std;
 
 class student
 {
-int addmno;
-string sname;
-int Class;
-char sec;
-int rno;
-public:
-void sinput()
-{
-cout<<"Enter Addmission No.:";
-cin>>addmno;
-cout<<"Enter Name of student:";
-getline(cin,sname);
-cout<<"Enter class of student:";
-cin>>Class;
-cout<<"Enter section of student:";
-cin>>sec;
-cout<<"Enter Roll of student:";
-cin>>rno;
-}
-void sdisplay()
-{
-cout<<"Name:"<<sname<<endl;
-cout<<"Class:"<<Class<<endl;
-cout<<"Section:"<<sec<<endl;
-cout<<"Addmission Number:"<<addmno<<endl;
-cout<<"Roll Number:"<<rno<<endl;
-}
-int getaddmno()
-{
-return addmno;
-}
+    int addmno;
+    std::string sname;
+    int Class;
+    char sec;
+    int rno;
+    
+    public:
+    void sinput();
+    void sdisplay();
+    int getaddmno();
 };
 
 #endif
